3ccaeb626d02a1b942981365e844dccfcd395325
Artur L <falcon.arthur@gmail.com>
2022-04-22 22:04:37 +0300

    465

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-document.querySelector(".card-text").innerHTML="Current color: rgb(240,255,240)";
+let BG = $("body").css("background-color");
+$(".card-text").text("Current color: " + BG);
+// document.querySelector(".card-text").innerHTML="Current color: " + BG;
 
 document.querySelector(".btn").addEventListener("click", function() {
     let RGB = "rgb(" + Math.floor(Math.random()*256) + "," + Math.floor(Math.random()*256) + "," + Math.floor(Math.random()*256) +")";
-    console.log(RGB)
-    document.body.style.backgroundColor = RGB;
-    document.querySelector(".card-text").innerHTML="Current color: " +RGB;
+    // console.log(RGB)
+    $("body").css("background-color", RGB)
+    // document.body.style.backgroundColor = RGB;
+    $(".card-text").text("Current color: " + RGB);
+    // document.querySelector(".card-text").innerHTML="Current color: " +RGB;
 });
 
 $("h1").css("color","red");
