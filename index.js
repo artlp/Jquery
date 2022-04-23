736e78c4bfd2597585295645b868028e3a70e44d
Artur L <falcon.arthur@gmail.com>
2022-04-23 13:53:18 +0300

    final

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let BG = $("body").css("background-color");
-$(".card-text").text("Current color: " + BG);
+$(".card-text").text("Current color: " + BG.toUpperCase());
 // document.querySelector(".card-text").innerHTML="Current color: " + BG;
 
-document.querySelector(".btn").addEventListener("click", function() {
+    $(".btn").click(function() {
+    // document.querySelector(".btn").addEventListener("click", function() {
     let RGB = "rgb(" + Math.floor(Math.random()*256) + "," + Math.floor(Math.random()*256) + "," + Math.floor(Math.random()*256) +")";
     // console.log(RGB)
-    $("body").css("background-color", RGB)
+    $("body").animate({backgroundColor:RGB}, 500);
     // document.body.style.backgroundColor = RGB;
-    $(".card-text").text("Current color: " + RGB);
+    $(".card-text").text("Current color: " + RGB.toUpperCase());
     // document.querySelector(".card-text").innerHTML="Current color: " +RGB;
 });
 
